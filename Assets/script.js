59e98f0b1bde5c64a9fd05bc4fe533949bc41a42
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-06-24 15:53:57 -0500

    timer added

--- Assets/script.js
+++ Assets/script.js
@@ -1,49 +1,56 @@
-var startButton = document.querySelector(".start-btn")
-var timerElement = document.querySelector(".timer");
+var startButton = document.querySelector(".start-btn");
+var questionText = document.querySelector(".question-content");
 
+var question = "";
 var isCorrect = false;
 var timer;
 var timerCount;
 
-var questions = ["What are the names of Annika's dogs?", "Who is Annika's favorite singer?", "What is Annika's current profession?"]
+//Arrays used to create questions and multiple choice answers on screen
+var question = [];
+var answerChoices = [];
+
+//Array of quiz questions
+var questions = ["What are the names of Annika's dogs?", "Who is Annika's favorite singer?", "What is Annika's current profession?"];
 
 //When I click the start button, a timer starts and a question appears
-
 function startQuiz() {
-    isCorrect = false;
-    timerCount = 100;
-    startButton.disabled = true;
-    startQuestions();
-   // startTimer();
+   isCorrect = false;
+   timerCount = 30;
+   startButton.disabled = true;
+   quizQuestion();
+   startTimer();
 }
 
-function startQuestions() {
-    return questions[0];
+function quizQuestion() {
+    
 }
+
+var timerCount = 30;
+var timerElement = document.querySelector(".timer");
+var timer = setInterval(startTimer, 1000);
 
 function startTimer() {
-    timer = setInterval(function() {
-        timerCount--;
-        timerElement.textContent = timerCount;
-    })
+    if (timerCount == 0) {
+        clearTimeout(timer);
+        endQuiz();
+    }
+    else {
+        timerElement.innerHTML = "Time Remaining: " + timerCount;
+        timerCount --;
+    }
+}
+
+function endQuiz() {
+
 }
 
 startButton.addEventListener("click", startQuiz)
 
-init();
-
-//When I answer a question, I am given another questions
+//When I answer a question, I am given another question
 
 //When I answer wrong, time subtracts from the clock
 
 //When the quiz is done or the timer runs out, the quiz is over
 
 //When the quiz is over, I can save my initials and score.
-
-function getCorrect() {
-
-}
-
-function getIncorrect() {
-
-}
