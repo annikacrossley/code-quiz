3b4cd90d766889dc8b7c6616f198fbd12ff18a5e
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-06-25 12:06:31 -0500

    add answer choice to buttons

--- Assets/script.js
+++ Assets/script.js
@@ -29,7 +29,7 @@ function startTimer() {
     }, 1000)
  }
 
- //Array used to create questions and multiple choice answers on screen
+//Array used to create questions and multiple choice answers on screen
 var quiz = [
     {
         question: "What are the names of Annika's dogs?",
@@ -48,19 +48,36 @@ var quiz = [
     }
 ]
 
-//When I answer a question, I am given another question
-var questionText = document.querySelector(".quiz");
+//Display first question
+    //Replace start quiz instruction w/ question - done 
+    //Reveal answer choice buttons - done
+    //Add answer choice text in A/B/C buttons 
+//When I answer a question, I am given another question    
+    //Add eventListener to each answer choice button
+    //Move to next question when an answer button has been clicked
+    //Store score of each answer selection as correct or incorrect
+
+var questionText = document.querySelector(".quiz-content");
+var optiona = document.querySelector(".a")
+var optionb = document.querySelector(".b")
+var optionc = document.querySelector(".c")
 var question = "";
 
 function askQuestion() {
     question = questionText.textContent;
     questionText.textContent = quiz[0].question;
-   console.log(quiz[0].question)
-   console.log(quiz[0].choices)
-   console.log(quiz[0].answer)
+    optiona.textContent=quiz[0].choices[0];
+    optionb.textContent=quiz[0].choices[1];
+    optionc.textContent=quiz[0].choices[2];
+    console.log(quiz[0].question)
+    console.log(quiz[0].choices)
+    console.log(quiz[0].answer)
 }
 
 //When I answer wrong, time subtracts from the clock
+    //Create an if/else ()
+    //If selected answer === correct answer, no change to time
+    //If selected answer != correct answer, take 5 seconds off the clock
 
 //When the quiz is done or the timer runs out, the quiz is over
 function endQuiz() {
