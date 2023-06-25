35365a882f885c03f3c0b5e24955cdecabfdfa7d
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-06-24 20:30:17 -0500

    askQuestion for loop started

--- Assets/script.js
+++ Assets/script.js
@@ -1,43 +1,19 @@
-var startButton = document.querySelector(".start-btn");
-var questionText = document.querySelector(".quiz-content");
-
-var question = "";
 var isCorrect = false;
-var timer;
-var timerCount;
-
-//Array of quiz questions & answers
-var quizQuestions = [
-    "What are the names of Annika's dogs?",
-    "Who is Annika's favorite singer?",
-    "What is Annika's current occupation?",
-];
-
-var answerChoices = ["a", "b", "c"]
 
 //When I click the start button, a timer starts and a question appears
+var startButton = document.querySelector(".start-btn");
 function startQuiz() {
-   isCorrect = false;
-   timerCount = 30;
-   startButton.disabled = true;
-   quizQuestion();
-   startTimer();
-}
-
-//Arrays used to create questions and multiple choice answers on screen
-var outputQuestion = [];
-var answerChoices = [];
-
-function quizQuestion() {
-    for(var i = 0; i < quizQuestions.length; i++) {
-        question = quizQuestions[i];
-        questionText.textContent = question;
-    }
-}
+    isCorrect = false;
+    timerCount = 30;
+    startButton.disabled = true;
+    askQuestion()
+    startTimer();
+ }
 
 var timerCount = 30;
 var timerElement = document.querySelector(".timer");
-
+var timer;
+var timerCount;
 function startTimer() {
     var timer = setInterval(function() {
         if (timerCount == -1) {
@@ -49,15 +25,38 @@ function startTimer() {
             timerCount --;
         }
     }, 1000)
+ }
+
+ //Arrays used to create questions and multiple choice answers on screen
+var quizQuestions = [
+    "What are the names of Annika's dogs?",
+    "Who is Annika's favorite singer?",
+    "What is Annika's current occupation?",
+];
+
+var quizAnswerA = ["Teague & Biscuit", "Harry Styles", "Dietitian"]
+var quizAnswerB = ["Biscuit & Suki", "Taylor Swift", "Engineer"]
+var quizAnswerC = ["Suki & Teague", "Ariana Grande", "Teacher"]
+
+var questionText = document.querySelector(".quiz-content");
+var question = "";
+
+function askQuestion() {
+    for(var i = 0; i < quizQuestions.length; i++) {
+        question = quizQuestions[i];
+        questionText.textContent = question;
+        return;
+    }
 }
 
 function endQuiz() {
 
 }
 
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
 
 //When I answer a question, I am given another question
+
 
 //When I answer wrong, time subtracts from the clock
 
