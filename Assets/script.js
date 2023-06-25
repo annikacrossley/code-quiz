83ed8a665de1b6d6cc13c13ef63bf201947156d6
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-06-25 14:48:24 -0500

    added styles

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,9 @@
 //When I click the start button, a timer starts and a question appears
 var startButton = document.querySelector(".start-btn");
 var choices = document.querySelector(".choices"); 
-    
+var timerCount;
+
+//Before startQuiz(), choices buttons are hidden. After startQuiz(), startButton is hidden and choices buttons appear. 
 choices.style.visibility = 'hidden';
 
 function startQuiz() {
@@ -12,14 +14,13 @@ function startQuiz() {
     startTimer();
  }
 
-var timerCount = 30;
 var timerElement = document.querySelector(".timer");
 var timer;
-var timerCount;
+
+//startTimer() begins when startQuiz() runs. endQuiz() runs when timer runs out.
 function startTimer() {
      timer = setInterval(function() {
         if (timerCount == -1) {
-          
             endQuiz();
         }
         else {
@@ -29,11 +30,11 @@ function startTimer() {
     }, 1000)
  }
 
-//Array used to create questions and multiple choice answers on screen
+//Array used to create questions, answer choices, and correct answers.
 var quiz = [
     {
         question: "What are the names of Annika's dogs?",
-        choices: ["Teague & Bailey", "Bailey and Suki", "Suki and Teague"],
+        choices: ["Teague and Bailey", "Bailey and Suki", "Suki and Teague"],
         answer: "Suki and Teague"
     },
     {
@@ -48,32 +49,24 @@ var quiz = [
     }
 ]
 
-//Display first question
-    //Replace start quiz instruction w/ question - done 
-    //Reveal answer choice buttons - done
-    //Add answer choice text in A/B/C buttons - done
-//When I answer a question, I am given another question    
-    //Add eventListener to each answer choice button
-    //Move to next question when an answer button has been clicked
-    //Store score of each answer selection as correct or incorrect
-
+//Variables for questions and answer choices.
 var questionText = document.querySelector(".quiz-content");
 var optiona = document.querySelector(".a")
 var optionb = document.querySelector(".b")
 var optionc = document.querySelector(".c")
-var question = "";
-var rightWrong = document.querySelector(".right-wrong")
-optiona.addEventListener("click", checkAnswer)
-optionb.addEventListener("click", checkAnswer)
-optionc.addEventListener("click", checkAnswer)
-
 var currentQuestion = 0
+var question = "";
+
+//Variables for determining score and recording score.
+var rightWrong = document.querySelector(".right-wrong")
+var saveScore = document.getElementById("scores")
+var saveButton = document.getElementById("saveUsername")
 var score = 0
 
-var saveScore = document.getElementById("scores")
+//Save button is hidden until endQuiz() runs. 
 saveScore.style.visibility = 'hidden'
-var saveButton = document.getElementById("saveUsername")
 
+//When I answer a question, I am given another question.
 function askQuestion() {
     question = questionText.textContent;
     questionText.textContent = quiz[currentQuestion].question;
@@ -85,15 +78,20 @@ function askQuestion() {
     console.log(quiz[currentQuestion].answer)
 }
 
+//When a "click" event occurs on the answer choice buttons, checkAnswer(event) runs. 
+    //The selected choice is compared to the correct answer to determine if the score is increased and to relay a message telling the user if they chose an incorrect or correct answer.
+//When I answer wrong, time subtracts from the clock.    
+    //The timer subtracts 5 seconds if user selects an incorrect answer.
+    //If there are no remaining questions, endQuiz() runs. 
 function checkAnswer(event) {
     var userSelection = event.target.textContent;
     console.log(userSelection)
     if(userSelection === quiz[currentQuestion].answer){
-        score +=5
+        score +=1
         rightWrong.textContent = "You got it right!"
     }else{
         timerCount -=5
-        rightWrong.textContent = "Incorrect"
+        rightWrong.textContent = "Not quite."
     }
     if(currentQuestion < quiz.length-1 ){
         currentQuestion++
@@ -103,28 +101,34 @@ function checkAnswer(event) {
     }
 }
 
-//When I answer wrong, time subtracts from the clock
-    //Create an if/else ()
-    //If selected answer === correct answer, no change to time
-    //If selected answer != correct answer, take 5 seconds off the clock
-
-//When the quiz is done or the timer runs out, the quiz is over
+//When the quiz is done (determined in "checkAnswer(event)") or the timer runs out (determined in "startTimer()"), the endQuiz() will run.
+    //Timer will stop.
+    //Score is recorded.
+    //Answer choices are hidden. The question is replaced with the final score. The saveScore text is visible. 
 function endQuiz() {
     clearInterval(timer);
     console.log(score)
     choices.style.visibility = 'hidden';
-    questionText.textContent = "Final Score: " + (timerCount + score)
+    questionText.textContent = "Final Score: " + (score) + " out of 3"
     saveScore.style.visibility = 'visible'
 }
 
 //When the quiz is over, I can save my initials and score.
+    //saveScore text becomes visible and directs the user to the View High Scores button, which links to a new page containing a table of stored scores w/ username (initals).
 saveButton.addEventListener("click", function(){
     var userName = document.getElementById("username").value
-    var storedScores = JSON.parse(localStorage.getItem("dashboard")) || []
+    var storedScores = JSON.parse(localStorage.getItem("scoreboard")) || []
     storedScores.push({
-        user:userName, score: (timerCount+score)
+        user:userName, score: (score)
     })
-    localStorage.setItem("dashboard", JSON.stringify(storedScores))
-    saveScore.innerText = "Scores saved to dashboard. Click on View HighScores"
+    localStorage.setItem("scoreboard", JSON.stringify(storedScores))
+    saveScore.innerText = "Scores saved to scoreboard. Click on View High Scores."
 } )
+
+//When button "Start Quiz" is clicked, startQuiz() runs.
 startButton.addEventListener("click", startQuiz);
+
+//When an answer choice button is clicked, checkAnswer() runs.
+optiona.addEventListener("click", checkAnswer)
+optionb.addEventListener("click", checkAnswer)
+optionc.addEventListener("click", checkAnswer)
